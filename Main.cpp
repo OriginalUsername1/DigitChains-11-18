c175279191adf8c0452facebcf91ae0a827b5a69
Yetterm <35743045+OriginalUsername1@users.noreply.github.com>
2018-11-09 18:13:27 -0500

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Coding competition problem 2, Fall 18
+Written by Mark Yetter on 11/9/18
+Written in C++ (GNU GCC compiler) using Code::Blocks
+*/
+
+string notAForLoop (string arr[], string, int)
+
+
+int main()
+{
+
+    //Static variable allocation
+    int numOfVals, i, j, k;
+
+    output = "";
+
+    cin >> numOfVals;
+
+    //Dynamic variable allocation
+    string values[numOfVals];
+
+    for (i=0;i<numOfVals;i++)
+    {
+        cin >> values[i];
+    }
+    
+    
+
+    return 0;
+}
+
