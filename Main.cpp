cb06312eb2c4626d3a4231b46d1033636d54359f
Yetterm <35743045+OriginalUsername1@users.noreply.github.com>
2018-11-09 19:13:24 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -17,45 +17,80 @@ int main()
 {
 
     //Static variable allocation
-    int numOfVals, i, j, k;
+    int numOfVals, i;
     stack <string> output;
 
     cin >> numOfVals;
 
     //Dynamic variable allocation
     string values[numOfVals];
+    string tempVals[numOfVals-1];
 
     for (i=0;i<numOfVals;i++)
     {
         cin >> values[i];
     }
 
-    charizard(&output, values, numOfVals);
+    output.push(values[0]);
+
+    for (i=0;i<numOfVals-1;i++)
+    {
+        tempVals[i] = values[i+1];
+    }
+
+    charizard(&output, tempVals, numOfVals-1);
+
+    while(!output.empty())
+    {
+        cout << output.top();
+        output.pop();
+
+        if (!output.empty())
+        {
+            cout << " ";
+        }
+    }
 
     return 0;
 }
 
 void charizard(stack <string>* out, string vals[], int length)
 {
-    string temp;
-    int i, j, k;
-    
+    string temp[length-1];
+    int i, j, k, l, m;
+
+    if (length == 1)
+    {
+        out->push(vals[0]);
+        return;
+    }
+
     for (i=0;i<length;i++)
     {//gets a value to compare
-        for (j=0;j<out.top().length();j++)
+        for (j=0;j<out->top().length();j++)
         {//gets the letters of the control
-            for (k=0;k<vals[i].length;k++)
+            for (k=0;k<vals[i].length();k++)
             {//gets the letters of the test
-                if (out.top()[j] == vals[i][k])
+                if (out->top()[j] == vals[i][k])
                 {
-                    out.push(vals[i]);
-                    
-                    
+                    out->push(vals[i]);
+
+                    m=0;
+                    for (l=0;l<length;l++)
+                    {//pulls the value we pushed out of the list
+                        if (l!=i)
+                        {
+                            temp[m] = vals[l];
+                            m++;
+                        }
+                    }
+
+                    charizard(out, temp, length-1);
                 }
             }
         }
     }
-    
+
     return;
 }
 
